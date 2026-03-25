ff640b97b6cbc9d3c60d9680669fdc8c18716972
huynvn <Huy.Nguyen@navigosgroup.com>
2026-03-25 16:09:46 +0700

    06 Thực hành phương thức POST

--- 01/server.js
+++ 01/server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 
+app.use(express.json());
+
 const courses = [
   { id: 1, name: "Course 1" },
   { id: 2, name: "Course 2" },
@@ -19,6 +21,22 @@ app.get("/api/courses/:id", (req, res) => {
   const course = courses.find((c) => c.id === parseInt(req.params.id));
   if (!course) return res.status(404).send("Course not found");
   res.send(course);
+});
+
+app.post("/api/courses/add", (req, res) => {
+  console.log(req.body);
+  const course = {
+    id: courses.length + 1,
+    name: req.body.name,
+  };
+  courses.push(course);
+  res.send(
+    JSON.stringify({
+      success: true,
+      message: "Course added successfully",
+      data: courses,
+    }),
+  );
 });
 
 const PORT = process.env.PORT || 4000;
